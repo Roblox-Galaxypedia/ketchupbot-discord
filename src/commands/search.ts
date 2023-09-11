27a63a40a7ed4eda0d41460a67670736b9ba43c5
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2023-09-11 01:07:15 +0000

    Fix search.ts to work
    Seems like the modal plugin got deprecated. Gonna use awaitmodalsubmit instead

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -39,33 +39,39 @@ export class SearchCommand extends Command {
         modal.addComponents(firstrow)
 
         await interaction.showModal(modal)
-    }
+        
+        interaction.awaitModalSubmit({ time: 60000, filter: (interaction) => interaction.customId === "search--query-modal" })
+        .then(async (interaction) => {
+            await interaction.deferReply({ ephemeral: true })
+            const input = interaction.fields.getTextInputValue("query")
 
-    public async modalRun(interaction: Discord.ModalSubmitInteraction) {
-        await interaction.deferReply({ ephemeral: true })
-        const input = interaction.fields.getTextInputValue("query")
+            const { query } = await fetch(`https://robloxgalaxy.wiki/api.php?action=query&format=json&list=search&srsearch=${encodeURIComponent(input)}&srlimit=5`).then(response => response.json())
 
-        const { query } = await fetch(`https://robloxgalaxy.wiki/api.php?action=query&format=json&list=search&srsearch=${encodeURIComponent(input)}&srlimit=5`).then(response => response.json())
+            if (query.search.length <= 0) return await interaction.editReply("No results found, please try again!")
 
-        if (query.search.length <= 0) return await interaction.editReply("No results found, please try again!")
+            const titles: string[] = []
 
-        const titles: string[] = []
+            query.search.forEach((result: any) => {
+                titles.push(result.title)
+            })
 
-        query.search.forEach((result: any) => {
-            titles.push(result.title)
+            const forembed = titles.map(val => {
+                return `• [${val}](https://robloxgalaxy.wiki/wiki/${encodeURIComponent(val)})`
+            })
+
+            const searchresults = new Discord.EmbedBuilder()
+                .setColor("#000000")
+                .setAuthor({ name: "Galaxypedia", iconURL: "https://cdn.discordapp.com/icons/913914198344482846/09b983b97f42ca793f39199554eaf0f7.png?size=1024" })
+                .setTitle(`Search results for **${input.toLowerCase().replace(/\w\S*/g, (txt) => { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() })}**:`)
+                .setDescription(`${forembed.join("\n\n")}\n\n[More...](https://robloxgalaxy.wiki/wiki/Special:Search?search=${encodeURIComponent(input)}&fulltext=1)`)
+                .setFooter({ text: "The new era of the Galaxy Wiki" })
+
+            await interaction.editReply({ embeds: [searchresults] })
         })
-
-        const forembed = titles.map(val => {
-            return `• [${val}](https://robloxgalaxy.wiki/wiki/${encodeURIComponent(val)})`
+        .catch(error => {
+            console.error(error)
+            interaction.editReply("An error occured while trying to search the Galaxypedia!")   
         })
-
-        const searchresults = new Discord.EmbedBuilder()
-            .setColor("#000000")
-            .setAuthor({ name: "Galaxypedia", iconURL: "https://cdn.discordapp.com/icons/913914198344482846/09b983b97f42ca793f39199554eaf0f7.png?size=1024" })
-            .setTitle(`Search results for **${input.toLowerCase().replace(/\w\S*/g, (txt) => { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() })}**:`)
-            .setDescription(`${forembed.join("\n\n")}\n\n[More...](https://robloxgalaxy.wiki/wiki/Special:Search?search=${encodeURIComponent(input)}&fulltext=1)`)
-            .setFooter({ text: "The new era of the Galaxy Wiki" })
-
-        await interaction.editReply({ embeds: [searchresults] })
+        
     }
 }
