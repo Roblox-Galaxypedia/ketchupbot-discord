96f86b6cd05244a31f888eb50adca88a5dca7032
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2023-09-11 01:01:08 +0000

    Fix indentation

--- src/listeners/commandDenied.ts
+++ src/listeners/commandDenied.ts
@@ -2,18 +2,18 @@
 import { Listener, UserError, ChatInputCommandDeniedPayload } from "@sapphire/framework"
 
 export class CommandDeniedListener extends Listener {
-  public constructor(context: Listener.Context, options: Listener.Options) {
-    super(context, {
-      ...options,
-      event: "chatInputCommandDenied",
-    })
-  }
+    public constructor(context: Listener.Context, options: Listener.Options) {
+        super(context, {
+            ...options,
+            event: "chatInputCommandDenied",
+        })
+    }
 
-  public async run(error: UserError, { interaction }: ChatInputCommandDeniedPayload) {
-    if (Reflect.get(Object(error.context), "silent")) return
-    return
+    public async run(error: UserError, { interaction }: ChatInputCommandDeniedPayload) {
+        if (Reflect.get(Object(error.context), "silent")) return
+        return
 
-    console.error(error.stack)
-    await interaction.reply({ content: error.message, ephemeral: true })
-  }
+        console.error(error.stack)
+        await interaction.reply({ content: error.message, ephemeral: true })
+    }
 }
