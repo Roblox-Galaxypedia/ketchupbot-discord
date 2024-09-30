18bfbb2a52f5754b4274fdcc3c7bdd9d72e648f0
smallketchup82 <ketchup@smallketchup.ca>
2024-09-30 01:05:13 -0400

    Catch error code 400 and provide a better error message

--- ketchupbot-discord/GalaxyGPT.cs
+++ ketchupbot-discord/GalaxyGPT.cs
@@ -154,6 +154,12 @@ public static class GalaxyGpt
 
             #endregion
         }
+        catch (HttpRequestException e)
+        {
+            // If the status code is not 400, rethrow the exception
+            if (e.StatusCode != HttpStatusCode.BadRequest) throw;
+            await message.ReplyAsync("So, you got moderated. Oh well. Blame OpenAI");
+        }
         catch (Exception e)
         {
             Console.WriteLine(e);
